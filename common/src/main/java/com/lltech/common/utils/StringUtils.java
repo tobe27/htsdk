3f8e783c68ee68497ea6915c9cc775f40dde877c
lichunyu <275937729@qq.com>
2019-04-09 17:29:49 +0800

    增加随机生成数字字母串

--- common/src/main/java/com/lltech/common/utils/StringUtils.java
+++ common/src/main/java/com/lltech/common/utils/StringUtils.java
@@ -38,6 +38,39 @@ public class StringUtils extends org.apache.commons.lang3.StringUtils {
     private StringUtils(){}
 
     /**
+     * 创建指定数量的随机字符串
+     * @param numberFlag 是否是数字
+     * @param length 不小于2
+     * @return 字符串
+     */
+    public static String createRandom(boolean numberFlag, int length) {
+        if (length < 2) {
+            throw new IllegalArgumentException("无效长度 {length=" + length + "},长度不小于2");
+        }
+        String retStr;
+        String strTable = numberFlag ? "1234567890" : "1234567890abcdefghijkmnpqrstuvwxyz";
+        int len = strTable.length();
+        boolean bDone = true;
+        do {
+            retStr = "";
+            int count = 0;
+            for (int i = 0; i < length; i++) {
+                double dblR = Math.random() * len;
+                int intR = (int) Math.floor(dblR);
+                char c = strTable.charAt(intR);
+                if (('0' <= c) && (c <= '9')) {
+                    count++;
+                }
+                retStr += strTable.charAt(intR);
+            }
+            if (count >= 2) {
+                bDone = false;
+            }
+        } while (bDone);
+        return retStr;
+    }
+
+    /**
      * 是否包含字符串
      *
      * @param str  验证字符串
